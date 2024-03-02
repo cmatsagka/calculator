84b0aeccfeb48af359abb0f0b328d3cc60eda44c
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2024-03-02 21:11:01 +0200

    Fix and simplify populateDisplay function

--- script.js
+++ script.js
@@ -11,71 +11,76 @@ const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        buttonPressed = button.id;
-        console.log(buttonPressed);
-        populateDisplay(buttonPressed);
-    });
+  button.addEventListener('click', () => {
+    buttonPressed = button.id;
+    console.log(buttonPressed);
+    displayValue = populateDisplay(buttonPressed);
+
+    if (isOperator(buttonPressed)) {
+      operatorN = buttonPressed;
+      numberFirst = +keys.slice(0, -1).join('');
+    }
+
+    // if (key === '=') {
+    //   operate(operatorN, numberFirst, numberSecond);
+    // }
+
+    console.log('displayValue=', displayValue);
+    console.log('numb1 = ', numberFirst);
+    console.log('op =', operatorN);
+  });
 });
 
-displayValue = populateDisplay();
+function populateDisplay(key) {
+  keys[i] = key;
+  i++;
 
-function populateDisplay(key){
-    keys[i] = key;
-    i++;
+  if (key === 'clear') {
+    keys = [];
+    i = 0;
+    display.textContent = 0;
+  } else if (key === 'x') {
+    display.textContent = keys.slice(0, -2).join('');
+    i = i - 2;
+  } else {
+    display.textContent = keys.join('');
+  }
 
-    if (isOperator(key)){
-        display.textContent += key + ' ';
-        displayValue = display.textContent;
-    }else if (key === '='){
-        display.textContent += ' ' + '=';
-        displayValue = +display.textContent;
-    }else if (key === 'clear'){
-        keys = [];
-        i = 0;
-        display.textContent = 0;
-        displayValue = 0;
-    }else if (key === 'x'){
-        display.textContent = keys.pop();
-        i = i - 2;
-        displayValue = +display.textContent;
-    }else{
-        display.textContent = keys.join('') + ' ';
-        displayValue = +display.textContent;
-    }
-    return displayValue;
+  displayValue = display.textContent;
+
+  return displayValue;
 }
 
 function operate(operator, numberA, numberB) {
-    if (operator === '+'){
-        return add(numberA, numberB);
-    }else if(operator === '-'){
-        return subtract(numberA, numberB);
-    }else if(operator === '*'){
-        return multiply(numberA, numberB);
-    }else if(operator === '/'){
-        return divide(numberA, numberB);
-    }else if(operator === '^'){
-        return power(numberA, numberB);
-    }
+  if (operator === '+') {
+    return add(numberA, numberB);
+  } else if (operator === '-') {
+    return subtract(numberA, numberB);
+  } else if (operator === '*') {
+    return multiply(numberA, numberB);
+  } else if (operator === '/') {
+    return divide(numberA, numberB);
+  } else if (operator === '^') {
+    return power(numberA, numberB);
+  }
 }
 
 function add(a, b) {
-    return a + b;
-};
+  return a + b;
+}
 
 function subtract(a, b) {
-    return a - b;
-};
+  return a - b;
+}
 
 function multiply(a, b) {
-    return a * b;
-};
+  return a * b;
+}
 
 function divide(a, b) {
-    return a / b;
-};
+  return a / b;
+}
 
 function power(a, b) {
-    return a ** b;
-};
+  return a ** b;
+}
