2cea291b48170f0b73f5beda9f16706118974859
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2025-12-10 23:15:39 +0200

    Remove console.logs

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let a;
-let b;
+let a = 0;
+let b = 0;
 let operator;
+let sum = 0;
 
 function add(a, b) {
     return a + b;
@@ -32,24 +33,48 @@ function operate(a, b, operator){
 }
 
 const calculation = document.querySelector('.calculation');
+const result = document.querySelector('.result');
 const btn = document.querySelectorAll('button');
+calculation.textContent = '';
 
-let number = calculation.textContent;
+let display = calculation.textContent;
+let firstNumber = false;
 
 btn.forEach(btn => {
     btn.addEventListener('click', populateDisplay => {
 
         let digit = btn.textContent;
 
-        if (Number.isInteger(parseInt(digit))){
-            calculation.textContent = number.concat(digit);
-            number = calculation.textContent;
+        if (Number.isInteger(parseInt(digit)) && !firstNumber){
+
+            display += digit;
+            calculation.textContent = display;
+            a += digit;
+
+            result.textContent = sum;
+        }else if (Number.isInteger(parseInt(digit)) && firstNumber){
+            console.log('sum b: ', sum);
+            display += digit;
+            calculation.textContent = display;
+            b += digit;
+
         }else{
-            a = number;
-            number = 0;
+            firstNumber = true;
+
             if (digit == '+' || digit == '-' || digit == '*' || digit == '/'){
+                console.log('sum op: ', sum);
                 operator = digit;
-                console.log(operator);
+                display += digit;
+                calculation.textContent = display;
+
+            }else if (digit == '='){
+                sum = operate(+a, +b, operator);
+                console.log('sum = ', sum);
+                display = ' = ' + sum;
+                result.textContent = display;
+                display = '';
+                firstNumber = false;
+                sum = 0;
             }
         }
     });
