cff6bc891ceb3ee8bbf9bb4b85d8d002f6e40cee
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2025-12-10 19:33:25 +0200

    Create function operate that calls the calculation functions

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let a;
+let b;
+let operator;
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +16,17 @@ function multiply(a, b){
 
 function divide(a, b){
     return a / b;
+}
+
+function operate(a, b, operator){
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        }
 }
